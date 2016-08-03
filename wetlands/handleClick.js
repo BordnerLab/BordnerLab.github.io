35a315e35a46353e2751a96f61710ce8b6239133
schuylerwilliams <skywilliams@gmail.com>
2016-08-03 15:16:32 -0500

    Moved everything to index

--- wetlands/handleClick.js
+++ wetlands/handleClick.js
@@ -1,25 +1,3 @@
 
 
 
-function determineClick(feature) {
-	if (feature.properties.COUNTY_NAM == "Ashland") {
-		Ashland();
-	} else {
-		alert(feature.properties.COUNTY_NAM);
-};
-
-function Ashland() {
-		map.addLayer({
-			'id': 'ashlandWetlands',
-			'type': 'fill',
-			'source': 'ashlandWetlands',
-			'layout': {},
-			'paint': {
-				'fill-color': '#f08',
-				'fill-opacity': 0.4
-			}
-		}, 'water');
-		
-		map.removeLayer('county-fills');
-		
-};
