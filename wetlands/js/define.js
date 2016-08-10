8bcee9a85b2251dcb296483fb830563f96795bb3
schuylerwilliams <skywilliams@gmail.com>
2016-08-10 11:55:59 -0500

    new js file

---
+++ wetlands/js/define.js
@@ -0,0 +1,85 @@
+var countyIDArr = [
+	"ashlandWetlands",
+	"bayfieldWetlands",
+	"brownWetlands",
+	"doorWetlands",
+	"douglasWetlands",
+	"ironWetlands",
+	"kenoshaWetlands",
+	"kewauneeWetlands",
+	"marinetteWetlands",
+	"ozaukeeWetlands",
+	"racineWetlands",
+	"sheboyganWetlands"
+];
+	
+var waterIDArr = [
+	"ashlandWater",
+	"bayfieldWater",
+	"brownWater",
+	"doorWater",
+	"douglasWater",
+	"ironWater",
+	"kenoshaWater",
+	"kewauneeWater",
+	"marinetteWater",
+	"ozaukeeWater",
+	"racineWater",
+	"sheboyganWater"
+];
+	
+var tempWaterLayers = [];
+	
+var countyNames = [
+	"Ashland",
+	"Bayfield",
+	"Brown",
+	"Door",
+	"Douglas",
+	"Iron",
+	"Kenosha",
+	"Kewaunee",
+	"Marinette",
+	"Ozaukee",
+	"Racine",
+	"Sheboygan"
+];
+	
+var currentLayers = [];
+	
+var tempLayers = [];
+	
+var hoverLayers = [
+	"ashlandWetlands",
+	"bayfieldWetlands",
+	"brownWetlands",
+	"doorWetlands",
+	"douglasWetlands",
+	"ironWetlands",
+	"kenoshaWetlands",
+	"kewauneeWetlands",
+	"marinetteWetlands",
+	"ozaukeeWetlands",
+	"racineWetlands",
+	"sheboyganWetlands",
+	"county-fills"
+];
+	
+var wetlandsLegendHolder;
+var wetlandsLegend = [
+	"A4",
+	"A3",
+	"B4",
+	"C4",
+	"C4b",
+	"F4",
+	"MF",
+	"ER"
+]
+	
+var tempMouseOverArr = [];
+	
+var fullDisplayControl = false;
+var selectByCountyControl = true;
+
+
