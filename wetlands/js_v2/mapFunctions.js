e0f5e74f77b893bd28f3f7157fd81e2205fe99ac
schuylerwilliams <skywilliams@gmail.com>
2016-08-17 16:33:32 -0500

    test

--- wetlands/js_v2/mapFunctions.js
+++ wetlands/js_v2/mapFunctions.js
@@ -37,11 +37,11 @@ function displayWater(id) {
 
 function takeScreenshot() {
 	//window.open(window.location.href, '_blank');
-	var data = map.getCanvas().toDataURL("image/png");
+	var data1 = map.getCanvas().toDataURL("image/png");
 	/*var canvas = map.getCanvas();
 	var dataURL = canvas.toDataURL("image/png");
 	*/
 	//console.log(data);
 	//window.location.href = data;
-	window.open(data);
+	window.open(data1);
 };
