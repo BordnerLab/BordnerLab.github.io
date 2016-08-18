025c2b3bb13bb511d2e664d10a5da79ffa1b618a
schuylerwilliams <skywilliams@gmail.com>
2016-08-18 16:16:31 -0500

    test

--- wetlands/js_v2/uiFunctions.js
+++ wetlands/js_v2/uiFunctions.js
@@ -27,12 +27,12 @@ var toolbarControl = 0;
 function toggleToolbar() {
 	if (toolbarControl == 0) {
 		document.getElementById("toolbar").style.left = "50px";
-		document.getElementById("grabHandleToggle").className = "fa fa-angle-left fa-2x";
+		document.getElementById("grabHandleToggle").className = "fa fa-angle-left fa-3x";
 		toolbarControl = 1;
 		return toolbarControl;
 	} else if (toolbarControl > 0) {
 		document.getElementById("toolbar").style.left = "0px";
-		document.getElementById("grabHandleToggle").className = "fa fa-angle-right fa-2x";
+		document.getElementById("grabHandleToggle").className = "fa fa-angle-right fa-3x";
 		toolbarControl = 0;
 		return toolbarControl;
 	}
