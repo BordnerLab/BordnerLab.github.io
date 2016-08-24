2b65961ce36e7850dbb254397a5cf1fa209125eb
Schuyler Williams <skywilliams@gmail.com>
2016-08-24 17:11:04 -0500

    organize

--- wetlands/js_v2/mapFunctions.js
+++ wetlands/js_v2/mapFunctions.js
@@ -2,31 +2,6 @@ function determineClick(feature) {
 	clickedCountyName = feature.properties.COUNTY_NAM;	
 };
 
-function displayLikeLegend(id) {
-	document.getElementById(id).style.border = "2px solid white";
-};
-
-function resetLikeLegend(id) {
-	document.getElementById(id).style.border = "1px solid #ddd";
-};
-
-
-
-var displayWaterControl = 0;
-function displayWater(id) {
-	if (displayWaterControl == 0) {
-		displayWaterControl = 1;
-		document.getElementById(id).style.background = "#fff";
-		document.getElementById(id).style.color = "black";
-		
-	} else if (displayWaterControl > 0) {
-		displayWaterControl = 0;
-		document.getElementById(id).style.background = "#004878";
-		document.getElementById(id).style.color = "#fff";
-		
-	}
-};
-
 
 
 function takeScreenshot() {
