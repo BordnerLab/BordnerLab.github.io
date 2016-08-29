04d7fa2216666e4dcd41105eeae0863f6889c06a
Schuyler Williams <skywilliams@gmail.com>
2016-08-29 15:03:07 -0500

    test

--- wetlands/js_v2/uiFunctions.js
+++ wetlands/js_v2/uiFunctions.js
@@ -1,8 +1,11 @@
 
+
+
 $('.scroll-pane').jScrollPane({
 	verticalDragMinHeight: 20,
 	verticalDragMaxHeight: 20
 });
+
 
 
 var api = $('.scroll-pane').data('jsp');
@@ -540,6 +543,23 @@ function clickLegendBack() {
 		}, 3000);
 	
 };
+
+
+var showMorePolyInfoControl = 0;
+function showMorePolyInfo(source) {
+	
+	if (showMorePolyInfoControl == 0) {
+		document.getElementById("upArrow").setAttribute("class", "fa fa-angle-double-down fa-2x");
+		document.getElementById("polyClickedInfoBox").style.transform = "translate(0, 0)";
+		
+		showMorePolyInfoControl = 1;
+	} else if (showMorePolyInfoControl > 0) {
+		document.getElementById("upArrow").setAttribute("class", "fa fa-angle-double-up fa-2x");
+		document.getElementById("polyClickedInfoBox").style.transform = "translate(0, 75px)";
+		
+		showMorePolyInfoControl = 0;
+	}
+}
 
 
 
