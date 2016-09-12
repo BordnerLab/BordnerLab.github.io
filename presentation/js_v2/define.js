2622146ce211329e29fda6aec1a5f1b5bf2da718
schuylerwilliams <skywilliams@gmail.com>
2016-09-12 15:28:30 -0500

    remove counties on presentation

--- presentation/js_v2/define.js
+++ presentation/js_v2/define.js
@@ -74,45 +74,9 @@ var threeDControl = false;
 
 
 
-function addMapSources() {
-	map.addSource('coastalCounties', {
-		'type': 'geojson',
-		'data': 'data/counties.geojson'
-	});
-	
-	map.addSource('single-point', {
-		'type': 'geojson',
-		'data': {
-			'type': 'FeatureCollection',
-			'features': []
-		}
-	});
-};
 
-function addCountyInitial() {
-	map.addLayer({
-		'id': 'county-borders',
-		'type': 'line',
-		'source': 'coastalCounties',
-		'layout': {
-			'visibility': 'visible'
-		},
-		'paint': {
-			'line-color': 'black',
-			'line-width': 1
-		}
-	});
-	
-	map.addLayer({
-		'id': 'point',
-		'source': 'single-point',
-		'type': 'circle',
-		'paint': {
-			'circle-radius': 10,
-			'circle-color': '#007cbf'
-		}
-	});
-};
+
+
 
 
 
