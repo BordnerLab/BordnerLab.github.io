6c5502ad9a54301ab11024e0b93c6bd4f8da69d6
schuylerwilliams <skywilliams@gmail.com>
2016-09-20 12:12:52 -0500

    clean up

--- presentation/js_v2/define.js
+++ presentation/js_v2/define.js
@@ -28,36 +28,8 @@ var landcoverLayers = [
 	"Unknown Cover"
 ];
 
-var mainLegend = [
-	["AG", "#E59966"],
-	["UR", "#C0392A"],
-	["BA", "#AFB7C0"],
-	["SL", "#FAD79E"],
-	["MF", "#A66ABE"],
-	["WL", "#5FAEE3"],
-	["DF", "#54C083"],
-	["CF", "#19703E"],
-	["O", "#76D6C3"],
-	["U", "#FADC70"]
-];
 
 
-
-var wetlandsLayers = [
-	['A3', '#fbb03b'],
-	['A4', '#223b53'],
-	['B4', '#e55e5e'],
-	['C4', '#3bb2d0'],
-	['C4b', '#ccc']
-];
-
-
-
-var waterDisplayControl = false;
-var wetlandsDisplayControl = true;
-
-var data;
-var getCanvasControl = false;
 
 
 var toggle01Control = false;
