301670f013dbcf21314e805d12551614c991e9e1
Schuyler Williams <skywilliams@gmail.com>
2016-10-05 13:44:05 -0500

    update zoom

--- wetlands/js_v2/map.js
+++ wetlands/js_v2/map.js
@@ -4,7 +4,7 @@ var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/bordnerwlei/cirf7wsrr0003g8nlogxrqxyr',
 	center: [-89.5, 44.5],
-	zoom: 6,
+	zoom: 8,
 	preserveDrawingBuffer: true,
 	hash: false,
 	pitch: 0.1
