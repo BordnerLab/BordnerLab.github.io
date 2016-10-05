631877ae793ce482a788325742e2c9f90ab78653
Schuyler Williams <skywilliams@gmail.com>
2016-10-05 13:46:17 -0500

    update zoom and center

--- wetlands/js_v2/map.js
+++ wetlands/js_v2/map.js
@@ -3,8 +3,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYm9yZG5lcndsZWkiLCJhIjoiY2lyZjd1a2tyMDA3dmc2b
 var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/bordnerwlei/cirf7wsrr0003g8nlogxrqxyr',
-	center: [-89.5, 44.5],
-	zoom: 8,
+	center: [-88.0198, 44.5192],
+	zoom: 9,
 	preserveDrawingBuffer: true,
 	hash: false,
 	pitch: 0.1
