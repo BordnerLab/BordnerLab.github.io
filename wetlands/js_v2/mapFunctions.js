d653774010784e42d3876f962b30b3a40f27bfe4
Schuyler Williams <skywilliams@gmail.com>
2016-10-05 14:11:11 -0500

    update function

--- wetlands/js_v2/mapFunctions.js
+++ wetlands/js_v2/mapFunctions.js
@@ -615,6 +615,7 @@ function changeLegendAndMapUnknown(){
 
 
 function clickSpecificLegend(source) {
+	alert(source);
 	var xm;
 	for(xm=0; xm < wetlandsLayers.length; xm++) {
 		placeMe = wetlandLayers[xm];
