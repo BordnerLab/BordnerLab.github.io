a739edd1da15c48ad61c107c555be652992a7059
Schuyler Williams <skywilliams@gmail.com>
2016-10-28 11:50:15 -0500

    update

--- videopresentation/js_v2/define.js
+++ videopresentation/js_v2/define.js
@@ -276,11 +276,6 @@ function addMapSources() {
 			'features': []
 		}
 	});
-	
-	map.addSource('pointMerge' , {
-		'type': 'geojson',
-		'data': 'data/points/pointMerge.geojson'
-	});
 };
 
 function addCountyInitial() {
@@ -300,16 +295,6 @@ function addCountyInitial() {
 	map.addLayer({
 		'id': 'point',
 		'source': 'single-point',
-		'type': 'circle',
-		'paint': {
-			'circle-radius': 10,
-			'circle-color': '#007cbf'
-		}
-	});
-	
-	map.addLayer({
-		'id': 'pointMerge',
-		'source': 'pointMerge',
 		'type': 'circle',
 		'paint': {
 			'circle-radius': 10,
