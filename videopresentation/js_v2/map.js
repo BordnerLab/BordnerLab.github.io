09a563d3439ed3814dcc0abdeb7ef8e9a1956618
Schuyler Williams <skywilliams@gmail.com>
2016-10-28 14:55:28 -0500

    test

--- videopresentation/js_v2/map.js
+++ videopresentation/js_v2/map.js
@@ -24,6 +24,12 @@ map.addControl(draw);
 var calcButton = document.getElementById('calculate');
 calcButton.onclick = function() {
 	alert('woohoo!');
+	var data = draw.getAll();
+	
+	var features = map.queryRenderedFeatures(data, { layers: hoverLayers});
+	
+	console.log(features);
+	console.log(features[0]);
 	/*
 	var data = draw.getAll();
 	if (data.feature.length > 0) {
